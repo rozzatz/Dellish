b7e04cd572fd88e4fb3f318dae3c49f9ae12f4e9
bruce <145066600+rozzatz@users.noreply.github.com>
2024-12-02 15:21:07 -0800

    tilemap fix plus camera mover

--- DellishPROJECT/Assets/Scripts/CAMERA FOLLOW.cs
+++ DellishPROJECT/Assets/Scripts/CAMERA FOLLOW.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class CAMERAFOLLOW : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+      public float FollowSpeed = 2f;
+    public float yOffset = 1f;
+    public Transform target;
+    private playercontroller Script;
     void Update()
     {
+       
+        
+            Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
+            transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
         
     }
 }
