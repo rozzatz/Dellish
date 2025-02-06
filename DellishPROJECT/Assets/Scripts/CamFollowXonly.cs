8c570dd6026527f8b20073a0ed5b993c8fe557f5
bruce <753825@student.dusd.net>
2025-02-06 09:51:00 -0800

    fixed softlovking

---
+++ DellishPROJECT/Assets/Scripts/CamFollowXonly.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollowXonly : MonoBehaviour
+{
+    public float FollowSpeed = 2f;
+    public float yOffset = 1f;
+    public Transform target;
+    private playercontroller Script;
+    void Update()
+    {
+
+
+        Vector3 newPos = new Vector3(target.position.x,  yOffset, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+
+    }
+}
