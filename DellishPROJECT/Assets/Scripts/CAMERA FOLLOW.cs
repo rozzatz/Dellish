2407dd2a030422e16d614264564db7e4f1d4f42d
bruce <145066600+rozzatz@users.noreply.github.com>
2025-02-06 20:53:12 -0800

    x offset for camera follow

--- DellishPROJECT/Assets/Scripts/CAMERA FOLLOW.cs
+++ DellishPROJECT/Assets/Scripts/CAMERA FOLLOW.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class CAMERAFOLLOW : MonoBehaviour
 {
       public float FollowSpeed = 2f;
-    public float yOffset = 1f;
+    public float yOffset = 1f;   
+    public float xOffset = 1f;
     public Transform target;
     private playercontroller Script;
     void Update()
     {
        
         
-            Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
+            Vector3 newPos = new Vector3(target.position.x + xOffset, target.position.y + yOffset, -10f);
             transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
         
     }
--- DellishPROJECT/Assets/Scripts/CamFollowXonly.cs
+++ DellishPROJECT/Assets/Scripts/CamFollowXonly.cs
@@ -6,13 +6,14 @@ public class CamFollowXonly : MonoBehaviour
 {
     public float FollowSpeed = 2f;
     public float yOffset = 1f;
+    public float xOffset = 1f;
     public Transform target;
     private playercontroller Script;
     void Update()
     {
 
 
-        Vector3 newPos = new Vector3(target.position.x,  yOffset, -10f);
+        Vector3 newPos = new Vector3(target.position.x + xOffset,  yOffset, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
 
     }
