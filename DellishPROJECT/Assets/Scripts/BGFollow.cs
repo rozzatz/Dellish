75a1e9e95473323e705d71f2777cddc6a4286721
Diego Alleyne <753825@student.dusd.net>
2025-02-25 14:35:44 -0800

    Level 3 And Serpah tut work done

--- DellishPROJECT/Assets/Scripts/BGFollow.cs
+++ DellishPROJECT/Assets/Scripts/BGFollow.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CamFollowXonly : MonoBehaviour
+public class BGFollow : MonoBehaviour
 {
     public float FollowSpeed = 2f;
     public float yOffset = 1f;
     public float xOffset = 1f;
+    public float xMove = 1f;
+    public float yMove = 1f;
     public Transform target;
     private playercontroller Script;
     void Update()
     {
 
 
-        Vector3 newPos = new Vector3(target.position.x + xOffset,  yOffset,-10f);
+        Vector3 newPos = new Vector3(target.position.x * xMove * xMove + xOffset * xMove, target.position.y * yMove + yOffset * yMove);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
 
     }
--- DellishPROJECT/Assets/Scripts/CAMERA FOLLOW.cs
+++ DellishPROJECT/Assets/Scripts/CAMERA FOLLOW.cs
@@ -7,13 +7,15 @@ public class CAMERAFOLLOW : MonoBehaviour
       public float FollowSpeed = 2f;
     public float yOffset = 1f;   
     public float xOffset = 1f;
+    public float xMove = 1f;
+    public float yMove = 1f;
     public Transform target;
     private playercontroller Script;
     void Update()
     {
        
         
-            Vector3 newPos = new Vector3(target.position.x + xOffset, target.position.y + yOffset,-10f);
+            Vector3 newPos = new Vector3(target.position.x * xMove * xMove + xOffset * xMove, target.position.y * yMove  + yOffset * yMove, -10f);
             transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
         
     }
